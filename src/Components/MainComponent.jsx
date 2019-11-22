364c7cb839c1dcdbb216ed71e9782e5946d37573
zyrocode <steamzyro@gmail.com>
2019-11-22 18:44:51 +0100

    1.0

--- src/Components/MainComponent.jsx
+++ src/Components/MainComponent.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Container, Row, Input } from 'reactstrap'
 import NavComponent from './NavComponent';
-import MovieList from './MovieList';
+import SingleMovie from './SingleMovie';
 
 class MainComponent extends Component {
     state = {
-        search: "Star Wars"
+        search: "Star Wars",
+        movies: []
     }
     
     render() {
@@ -14,14 +15,26 @@ class MainComponent extends Component {
                 <Container fluid>
 
                     <NavComponent />
-                    <Input type="text" value={this.state.search} onChange={(val) => this.setState({search: val.target.value})} placeholder="Type a movie" />
+                    <Input type="text" value={this.state.search} onChange={(val) => this.setState({search: val.target.value}) } placeholder="Type a movie" />
                         <h1>TV Shows</h1>
                     <Row>
-                        <MovieList search={this.state.search}/>
+                        {this.state.movies
+                            .map((movie, index) =>
+                                movie.Poster !== "N/A" &&
+                                (<SingleMovie singleMovie={movie}/>)
+                            )}
                     </Row>
                 </Container>
             </>
         );
+    }
+
+    componentDidMount = async () => {
+        let response = await fetch("http://www.omdbapi.com/?apikey=fd3da7b9&s=" + this.state.search)
+        let movies = await response.json()
+        this.setState({
+            movies: movies.Search
+        })
     }
 }
 
